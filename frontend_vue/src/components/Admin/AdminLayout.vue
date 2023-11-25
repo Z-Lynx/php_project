86d0ffe669fad9ec534b722afde9f64e6ec52c58
ZLynX <nguyentrantien752@gmail.com>
2023-11-25 12:09:34 +0700

    fix UI admin

--- frontend_vue/src/components/Admin/AdminLayout.vue
+++ frontend_vue/src/components/Admin/AdminLayout.vue
@@ -1,12 +1,16 @@
 <template>
-    <HeaderAdmin />
-    <div class="flex-grow">
+  <div class="flex h-screen overflow-hidden">
+    <SideBar></SideBar>
+    <div class="relative w-full grow overflow-auto">
+      <div class="sticky top-0" style="z-index: 999">
+        <HeaderAdmin />
+      </div>
       <RouterView />
     </div>
-    <Footer />
+  </div>
 </template>
 
 <script setup>
 import HeaderAdmin from "./HeaderAdmin.vue";
-import Footer from "../Footer.vue";
+import SideBar from "./SideBar.vue";
 </script>
--- frontend_vue/src/components/Admin/HeaderAdmin.vue
+++ frontend_vue/src/components/Admin/HeaderAdmin.vue
@@ -4,11 +4,13 @@ import ProfileAdmin from "./ProfileAdmin.vue";
 </script>
 
 <template>
-  <header class="container flex items-center">
-    <Logo />
-    
-    <div class="flex w-full items-center justify-end space-x-12">
+  <div class=" flex justify-between items-center z-30 w-full bg-white border-b">
+    <div class="grow">
+      <Logo />
+    </div>
+
+    <div class="flex justify-end p-2 md:mt-0 md:p-0 md:pr-2 items-center space-x-12">
       <ProfileAdmin />
     </div>
-  </header>
+  </div>
 </template>
